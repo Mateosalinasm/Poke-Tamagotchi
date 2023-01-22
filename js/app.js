3fd8f93e3d3e187afd48ded50777df6494e4d83e
Mateo Salinas <mateo.salinas95@gmail.com>
2023-01-21 23:40:31 -0600

    Added an interval to the game

--- js/app.js
+++ js/app.js
@@ -1,74 +1,89 @@
+//Display Hunger, Play, Sleep
+const hungerScore = document.querySelector('.hungerScore')
+const playScore = document.querySelector('.playScore')
+const sleepScore = document.querySelector('.sleepScore')
+
+
+
+//Class
 class Tamagotchi {
     constructor(name){
         this.name = name
         this.hunger = 0
-        this.boredom = 0
-        this.fatigue = 0
+        this.play = 0
+        this.sleep = 0
+        setInterval(() => {
+            this.hunger += 1
+            this.play += 1
+            this.sleep += 1 
+            hungerScore.innerHTML = this.hunger
+            playScore.innerHTML = this.play
+            sleepScore.innerHTML = this.sleep
+        }, 2000)
     }
+   
 }
 
+const tama = this.name
 this.name = prompt('What\'s your Tamagotchis name?')
 const nameID = document.querySelector('.name')
-nameID.innerHTML = `${this.name}`
-alert(`Your new friend ${this.name} is born! Take good care of him`)
+nameID.innerHTML = `${tama}`
+alert(`Your new friend ${tama} is born! Take good care of him`)
 
 //New Pet
 
-const newPet = new Tamagotchi(this.name)
+const newPet = new Tamagotchi(tama)
+
+
+//Functions
+function petFeed (e){
+    e.preventDefault()  
+    this.hunger--  
+    console.log(`${newPet.name} has been fed and feels much better! Hunger level = ${this.hunger}` )
+    if(this.hunger > 5){
+        alert(`${tama} is getting awfully hungry, feed him before he gets sick!`)
+    } else if(this.hunger < 0){
+        this.hunger = 0
+    } else if(this.hunger > 10){
+        alert(`${tama} starved to death ☠️`)
+    }
+    
+}
+function petPlay (e){
+    e.preventDefault()
+    this.play--   
+    console.log(`${newPet.name} has played and had so much fun! Boredom level = ${this.play}` ) 
+    if(this.play > 5){
+        alert(`${tama} is getting bored, play with him before he gets depressed!`)
+    } else if(this.play < 0){
+        this.play = 0
+    } else if(this.play > 10){
+        alert(`${tama} got depressed and died ☠️`)
+    }
+
+}
+function petSleep (e){
+    e.preventDefault() 
+    this.sleep--    
+    console.log(`${newPet.name} has slept and feels refreshed! Fatigue level = ${this.sleep}` )
+    if(this.sleep > 5){
+        alert(`${tama} is getting sleepy, let him take a nap before he gets insomnia!`)
+    } else if(this.sleep < 0){
+        this.sleep = 0
+    } else if(this.sleep > 10){
+        alert(`${tama} got insomnia and died ☠️`)
+    }
+}
 
 
 //Buttons›
-
 let feed = document.querySelector('.feed')
 let play = document.querySelector('.play')
 let sleep = document.querySelector('.sleep')
-
 
 //Event Listeners
 feed.addEventListener('click', petFeed)
 play.addEventListener('click', petPlay)
 sleep.addEventListener('click', petSleep)
 
-
-//Functions
-function petFeed (e){
-    e.preventDefault()  
-    this.hunger -= 1  
-    console.log(`${newPet.name} has been fed and feels much better! Hunger level = ${this.hunger}` )
-    if(this.hunger > 5){
-        alert(`${this.name} is getting awfully hungry, feed him before he gets sick!`)
-    } else if(this.hunger < 0){
-        this.hunger = 0
-    } else if(this.hunger > 10){
-        alert(`${this.name} starved to death ☠️`)
-    }
-    
-}
-
-function petPlay (e){
-    e.preventDefault()   
-    console.log(`${newPet.name} has played and had so much fun! Boredom level = ${this.boredom}` )
-    if(this.boredom > 5){
-        alert(`${this.name} is getting bored, play with him before he gets depressed!`)
-    }else if(this.boredom > 10){
-        alert(`${this.name} got depressed and died ☠️`)
-    }
-
-}
-
-function petSleep (e){
-    e.preventDefault()    
-    console.log(`${newPet.name} has slept and feels refreshed! Fatigue level = ${this.fatigue}` )
-    if(this.fatigue > 5){
-        alert(`${this.name} is getting sleepy, let him take a nap before he gets insomnia!`)
-    }else if(this.fatigue > 10){
-        alert(`${this.name} got insomnia and died ☠️`)
-    }
-}
-
-//Counters
-
-// let feedCount = []
-// let sleepCount = []
-// let playCount = []
 
